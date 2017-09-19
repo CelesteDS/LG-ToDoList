b665db2dd535dc88a3317cbf03248a551b69bedb
Celeste De Santiago <celestedaphne@gmail.com>
2017-09-18 17:05:08 -0700

    Added working delete method

--- commands/delete.js
+++ commands/delete.js
@@ -3,14 +3,14 @@ const fs = require('fs');
 const deleting = function(tdList, idNum){
 //grab the list
   let tasks = JSON.parse(fs.readFileSync(tdList, 'utf8'));
-//check to make sure they entered a valid id number
+//check to make sure they entered a id number from the list
   let ind = -1;
   for(let i = 0; i <tasks.length; i++){
     if(tasks[i]['id']==idNum){
       ind = i;
     }
   }
-
+  //
   if(ind>0){
     console.log(`Deleted task ${idNum}: ${tasks[ind]['task']}`);
     tasks.splice(ind, 1);
