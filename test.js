52b8f37ecb5fc42fc0362a9456034fe636f1415f
Celeste De Santiago <celestedaphne@gmail.com>
2017-09-21 18:12:53 -0700

    Mocha tests confirming each function works!

--- test.js
+++ test.js
@@ -1,56 +1,73 @@
 const expect = require('chai').expect;
 const fs = require('fs');
-const add = require("./commands/add.js");
-const list = require("./commands/list.js");
-const delety = require("./commands/delete.js");
-const complete = require("./commands/complete.js");
-const { exec } = require("child_process");
+const add = require('./commands/add.js');
+const list = require('./commands/list.js');
+const delety = require('./commands/delete.js');
+const complete = require('./commands/complete.js');
+// const { exec } = require('child_process');
+
 const testArr = ['test1', 'test2', 'test3', 'test4'];
 const testFile = 'testTasks.json';
-const fnToAr = function (filename) {
-	return JSON.parse(fs.readFileSync(filename, 'utf8'))
+const testFile2 = 'testTasks2.json'
+const fnToAr = function fnToAr(filename) {
+  return JSON.parse(fs.readFileSync(filename, 'utf8'))
 }
 
 describe('add', () => {
-	it('should create a new JSON file', (done) => {
-	    testArr.forEach(function(elem) {
-		add(testFile, elem);
-	    })
-	    expect(fs.existsSync('testTasks.json')).to.be.true;
-	    done();
-         })
-	it('should put test1 into the JSON', (done) => {
-	  expect(fnToAr(testFile)[1].task).to.eql('test1');
-	  done();  
-	})
+  before(() => {
+    testArr.forEach(function (elem) {
+      add(testFile, elem);
+    })
+  })
+  it('should create a new JSON file', (done) => {
+    expect(fs.existsSync('testTasks.json')).to.be.true;
+    done();
+  })
+  it('should put test1 into the JSON', (done) => {
+    expect(fnToAr(testFile)[1].task).to.eql('test1');
+    done();
+  })
 })
 
 describe('complete', () => {
-	it('should mark the given task as complete', (done) => {
-	    complete(testFile, 2);
-	    expect(fnToAr(testFile)[2].complete).to.be.true;
-	    done();
-	})
+  it('should mark the given task as complete', (done) => {
+    complete(testFile, 2);
+    expect(fnToAr(testFile)[2].complete).to.be.true;
+    done();
+  })
 })
 
 describe('delety', () => {
-	it('should delete the given task', (done) => {
-	    delety(testFile, 3);
-	    expect(fnToAr(testFile)[3].task).to.eql('test4');
-	    done();
-
-	})
+  it('should delete the given task', (done) => {
+    delety(testFile, 3);
+    expect(fnToAr(testFile)[3].task).to.eql('test4');
+    done();
+  })
 })
 
 describe('list', function () {
-	let output = ''
-	list() > output;
-	it('should list the tasks', () => {
-	    console.log(`The output is ${output}`);
-	    it('shouldnt list the complete task', () => {
-	    })
-	    it('shouldnt list the deleted task', () => {
-	    })	
-	})
-})
+  let printedList = ''
+  before(() => {
+    testArr.forEach(function (elem) {
+      add(testFile2, elem);
+    })
+    complete(testFile2, 2);
+    delety(testFile2, 3);
+    printedList = list(testFile2);
+  })
 
+  it('should list the tasks', (done) => {
+    expect(printedList).to.include('test4');
+    done();
+  })
+
+  it('shouldnt list the completed task', (finish) => {
+    expect(printedList).to.not.include('test2');
+    finish();
+  })
+
+  it('shouldnt list the deleted task', (finish) => {
+    expect(printedList).to.not.include('test3');
+    finish();
+  })
+})
